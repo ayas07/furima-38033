3fd1b75d875949399642e5246ef9de6c3938fcb0
ayas07 <ayakasugiyama07@gmail.com>
2022-06-25 13:41:31 +0900

    商品出品機能

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+function sellPrice() {
+
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+
+
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+    const tax = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = tax;
+    profitDom.innerHTML = Math.floor(inputValue - tax);  
+ })
+}
+
+window.addEventListener('load', sellPrice)
---
+++ app/models/item.rb
@@ -0,0 +1,32 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_area
+  belongs_to :delivery_day
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+  end
+
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'は半角数字を使用してください' }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                            message: 'は¥300~¥9,999,999の間で入力してください' }
+end
